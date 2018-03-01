8f70a44f081f3700bce11469c0bb0c74a6b6b7b3
nickjosi <nickjosiahpark@gmail.com>
2018-03-01 08:41:07 -0700

    prog2

--- firmware_mpgl1/application/mpgl1_board_test.h
+++ firmware_mpgl1/application/mpgl1_board_test.h
@@ -24,8 +24,8 @@ Constants / Definitions
 
 /* BoardTest ANT radio settings */
 /* MAKE SURE TO CHANGE THE u8 and u32 values for DEVICEID */
-#define	ANT_DEVICEID_LO_BOARDTEST			        (u8)0x19
-#define ANT_DEVICEID_HI_BOARDTEST		          (u8)0x25
+#define	ANT_DEVICEID_LO_BOARDTEST	          (u8)0x19
+#define ANT_DEVICEID_HI_BOARDTEST		  (u8)0x25
 #define ANT_DEVICEID_DEC_BOARDTEST	          (u32)9497
 
 /* IF YOU CHANGE DEVICE TYPE OR TRANSMISSION TYPE, YOU MUST CHANGE 
